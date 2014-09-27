e004bb6f00b316fda793bd70e97901064fccf7fe
Funergy <niels.dewulf48@gmail.com>
2014-09-27 17:46:34 +0200

    Added the author

--- BungeePM/src/com/Funergy/BPM/BungeePM.java
+++ BungeePM/src/com/Funergy/BPM/BungeePM.java
@@ -2,7 +2,11 @@ package com.Funergy.BPM;
 
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.plugin.Plugin;
-
+/**
+ * 
+ * @author Funergy
+ *
+ */
 public class BungeePM extends Plugin{
 	
 	@Override
